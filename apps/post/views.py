5501cfaaeabef13be3ab7521abd50ec74ddeda58
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-09 18:40:42 -0300

    feat: created viewsets and serializers Post

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from apps.post.models import Post
+from apps.post.serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSets(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
