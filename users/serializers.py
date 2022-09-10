3a838bb2d7a6f98da8dc0d5ef53cca47b79d4c3e
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-10 16:31:42 -0300

    feat: created serializers and view UserAccount

---
+++ users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from users.models import UserAccount
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = ["first_name", "last_name", "email", "password"]
+
+    def validate(self, data):
+        user = UserAccount(**data)
+        password = data.get("password")
+
+        try:
+            validate_password(password=password, user=user)
+        except ValidationError as error:
+            serializers_errors = serializers.as_serializer_error(error)
+            raise ValidationError(
+                {"password": serializers_errors["non_field_errors"]}
+            ) from error
+
+        return data
+
+    def create(self, validated_data):
+        return UserAccount.objects.create_user(**validated_data)
+
+
+class GetUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = ["first_name", "last_name", "email"]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.serializers import GetUserSerializer, RegisterUserSerializer
+
+
+class RegisterUserView(APIView):
+    def post(self, request: Request):
+        data = request.data
+        serializer = RegisterUserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data="Successfully", status=status.HTTP_201_CREATED)
+
+
+class GetUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: Request):
+        user = request.user
+        user = GetUserSerializer(user)
+
+        return Response(data=user.data, status=status.HTTP_200_OK)
