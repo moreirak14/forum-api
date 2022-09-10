e044ea88df107be34f2b96ffdad3983dd424fbfe
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-10 18:28:41 -0300

    feat: added method create in PostViewSets

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.post.models import Post
@@ -7,3 +10,13 @@ from apps.post.serializers import PostSerializer
 class PostViewSets(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        if request.user.is_anonymous:
+            return
+
+        data = request.data
+        serializer = PostSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
