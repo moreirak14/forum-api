68c8507e6a46579f434dbd61d926cd6048606d1a
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-09 23:55:28 -0300

    feat: created and migrate model Comment

--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from apps.post.models import Post
+
+
+class Comment(models.Model):
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment of the post: {self.post}"
