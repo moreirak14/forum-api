d60340888e1b74a645829b388c3c4ed74dbe6f2d
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-10 00:08:05 -0300

    feat: created viewsets and serializers Comment

--- apps/comment/views.py
+++ apps/comment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from apps.comment.models import Comment
+from apps.comment.serializers import CommentSerializer
+
+
+class CommentViewSets(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
